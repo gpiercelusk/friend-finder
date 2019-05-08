676a737eb53c39b0c6bacf975d0cceb2c13b35b4
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-05-07 20:46:16 -0400

    added friend objects

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friendMatch = [
+  {
+    name: "Steve Rogers",
+    photo: "https://vignette.wikia.nocookie.net/marvel-cinematic/images/3/32/Steve_Rogers_2.jpg/revision/latest/scale-to-width-down/250?cb=20131025030358",
+    scores: ["4", "3", "4", "5", "2"]
+  }, {
+    name: "Tony Stark",
+    photo: "https://cdn.images.express.co.uk/img/dynamic/36/590x/Avengers-Endgame-Tony-Stark-end-scene-secret-meaning-1122911.webp?r=1557046902234",
+    scores: ["2", "5", "2", "2", "5"]
+  }, {
+    name: "Donald Blake",
+    photo: "https://media.npr.org/assets/img/2017/10/27/tra2016_comp_v005_008088.1111_wide-bc335cede10d086ba948db2d66f0093d36f0cc1e-s800-c85.jpg",
+    scores: ["4", "1", "4", "5", "1"]
+  }, {
+    name: "Peter Parker",
+    photo: "https://i.pinimg.com/originals/0d/6d/30/0d6d30abdecf7354f20d0ab7bd3da204.jpg",
+    scores: ["3", "3", "3", "3", "5"]
+  },
+
+];
+
+
+
+module.exports = friendMatch;
